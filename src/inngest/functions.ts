da501dac83bb4b4ceb284102479df10510f37c41
Vinay-mor <vinaymor2005@gmail.com>
2026-02-26 17:34:19 +0530

    AI Providers

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,35 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import {generateText} from "ai";
+import {createOpenAI} from "@ai-sdk/openai";
+import {createAnthropic} from "@ai-sdk/anthropic"
 
-export const helloWorld = inngest.createFunction(
-    { id: "hello-world" },
-    { event: "test/hello.world" },
+const google=createGoogleGenerativeAI();
+const openai=createOpenAI();
+const anthropic=createAnthropic();
+export const execute = inngest.createFunction(
+    { id: "execute-ai" },
+    { event: "execute/ai" },
     async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "1s");
-        await step.run("create-workflow", () => {
-            return prisma.workflow.create({
-                data: {
-                    name: "workflow-from-inngest",
-                },
-            });
+        await step.sleep("pretend",10000)
+        const {steps:geminiSteps}=await step.ai.wrap("gemini-generate-text",generateText,{
+            model:google("gemini-3-flash-preview"),
+            system:"You are a helpful assistant.",
+            prompt:"What is 4+4?"
         });
+        const {steps:openaiSteps}=await step.ai.wrap("openai-generate-text",generateText,{
+            model:openai("gpt-4"),
+            system:"You are a helpful assistant.",
+            prompt:"What is 4+4?"
+        });
+        const {steps:anthropicSteps}=await step.ai.wrap("anthropic-generate-text",generateText,{
+            model:anthropic("claude-opus-4-6"),
+            system:"You are a helpful assistant.",
+            prompt:"What is 4+4?"
+        });
+        return {
+            geminiSteps,openaiSteps,anthropicSteps,
+        }
     },
 );
