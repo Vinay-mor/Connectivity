833c0f2ccd2174b54a507bb665945562c20ce575
Vinay-mor <vinaymor2005@gmail.com>
2026-03-01 11:42:35 +0530

    Workflows crud

---
+++ src/features/workflows/components/workflows.tsx
@@ -0,0 +1,56 @@
+"use client"
+import { EntityContainer, EntityHeader } from "@/components/entity-components";
+import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/use-workflows"
+import React from "react";
+import { useUpgradeModel } from "@/hooks/use-upgrade-modal";
+import { useRouter } from "next/navigation";
+
+export const WorkflowsList = () => {
+    const workflows = useSuspenseWorkflows();
+    return (
+        <div className="flex-1 flex justify-center items-center">
+            <p>
+                {JSON.stringify(workflows.data, null, 2)}
+            </p>
+        </div>
+    )
+};
+export const WorkflowsHeader = ({ disabled }: { disabled?: boolean }) => {
+    const createWorkflow = useCreateWorkflow();
+    const router=useRouter();
+    const { handleError, modal } = useUpgradeModel();
+    const handleCreate = () => {
+        createWorkflow.mutate(undefined, {
+            onSuccess:(data)=>{
+                router.push(`/workflow/${data.id}`);
+            },
+            onError: (error) => {
+                handleError(error);
+            },
+        })
+    }
+    return (
+        <>
+            {modal}
+            <EntityHeader
+                title="Workflows"
+                description="Create and manage your workflows"
+                onNew={handleCreate}
+                newButonLabel="New workflow"
+                disabled={disabled}
+                isCreating={createWorkflow.isPending}
+            />
+        </>
+    )
+}
+export const WorkflowsContainer = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <EntityContainer
+            header={<WorkflowsHeader />}
+            search={<></>}
+            pagination={<></>}
+        >
+            {children}
+        </EntityContainer>
+    )
+}
---
+++ src/hooks/use-upgrade-modal.tsx
@@ -0,0 +1,19 @@
+import { UpgradeModel } from "@/components/upgrade-modal";
+import { TRPCClientError } from "@trpc/client";
+import { useState } from "react"
+
+export const useUpgradeModel=()=>{
+    const [open,setOpen]=useState(false);
+
+    const handleError=(error:unknown)=>{
+        if(error instanceof TRPCClientError){
+            if(error.data?.code==="FORBIDDEN"){
+                setOpen(true);
+                return true;
+            }
+        }
+        return false;
+    }
+    const modal=<UpgradeModel open={open}onOpenChange={setOpen}/>
+    return{handleError,modal};
+}
