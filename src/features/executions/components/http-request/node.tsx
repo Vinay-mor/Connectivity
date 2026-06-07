862f5fdd037a6298d13ba39755a7eae685b0f969
Vinay-mor <vinaymor2005@gmail.com>
2026-06-07 14:27:36 +0530

    node execution

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -4,14 +4,11 @@ import { useReactFlow, type Node, type NodeProps, } from "@xyflow/react"
 import { GlobeIcon } from "lucide-react"
 import { memo, useState } from "react";
 import { BaseExecutionNode } from "../base-execution-node";
-import { FormType, HttpRequestDialog } from "./dialog";
-import { nodeComponents } from "@/config/node-components";
-
+import { HttpRequestFormValues, HttpRequestDialog } from "./dialog";
 type HttpRequestNodeData = {
     endpoint?: string;
     method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
-    [key: string]: unknown;
 };
 
 type HttpRequestNodeType = Node<HttpRequestNodeData>;
@@ -25,7 +22,7 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
         : "Not Configured"
     const nodeStatus = "initial"
     const handleOpenSettings = () => setDialogOpen(true);
-    const handleSubmit = (values: FormType) => {
+    const handleSubmit = (values: HttpRequestFormValues) => {
         setNodes((nodes) => nodes.map((node) => {
             if (node.id === props.id) {
                 return {
@@ -44,9 +41,7 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
     return (
         <>
             <HttpRequestDialog open={dialogOpen} onOpenChange={setDialogOpen} onSubmit={handleSubmit}
-                defaultEndpoint={nodeData.endpoint}
-                defaultMethod={nodeData.method}
-                defaultBody={nodeData.body}
+                defaultValues={nodeData}
             />
             <BaseExecutionNode
                 {...props}
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,54 +1,47 @@
-import prisma from "@/lib/db";
+import { NonRetriableError } from "inngest";
 import { inngest } from "./client";
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import {generateText} from "ai";
-import {createOpenAI} from "@ai-sdk/openai";
-import {createAnthropic} from "@ai-sdk/anthropic"
-import * as Sentry from "@sentry/nextjs";
+import prisma from "@/lib/db";
+import { topologicalSort } from "./utils";
+import { NodeType } from "@/generated/prisma/enums";
+import { getExecutor } from "@/features/executions/lib/executor-registry";
 
-const google=createGoogleGenerativeAI();
-const openai=createOpenAI();
-const anthropic=createAnthropic();
-export const execute = inngest.createFunction(
-    { id: "execute-ai" },
-    { event: "execute/ai" },
+
+export const executeWorkflow = inngest.createFunction(
+    { id: "execute-workflow" },
+    { event: "workflows/execute.workflow" },
     async ({ event, step }) => {
-        await step.sleep("pretend","5s")
-        Sentry.logger.info("User triggered test log",{log_source:"sentry_test"})
-        console.warn("Something is missing");
-        console.error("This is an error i want to track");
-        const {steps:geminiSteps}=await step.ai.wrap("gemini-generate-text",generateText,{
-            model:google("gemini-2.5-flash-lite-preview-09-2025"),
-            system:"You are a helpful assistant.",
-            prompt:"What is 4+4?",
-            experimental_telemetry:{
-                isEnabled:true,
-                recordInputs:true,
-                recordOutputs:true,
-            },
-        });
-        const {steps:openaiSteps}=await step.ai.wrap("openai-generate-text",generateText,{
-            model:openai("gpt-4"),
-            system:"You are a helpful assistant.",
-            prompt:"What is 4+4?",
-            experimental_telemetry:{
-                isEnabled:true,
-                recordInputs:true,
-                recordOutputs:true,
-            },
-        });
-        const {steps:anthropicSteps}=await step.ai.wrap("anthropic-generate-text",generateText,{
-            model:anthropic("claude-opus-4-6"),
-            system:"You are a helpful assistant.",
-            prompt:"What is 4+4?",
-            experimental_telemetry:{
-                isEnabled:true,
-                recordInputs:true,
-                recordOutputs:true,
-            },
-        });
-        return {
-            geminiSteps,openaiSteps,anthropicSteps,
+        const workflowId=event.data.workflowId;
+
+        if(!workflowId){
+            throw new NonRetriableError("workflow ID is missing");
         }
+       const sortedNodes=await step.run("prepare-workflow",async()=>{
+        const workflow=await prisma.workflow.findUniqueOrThrow({
+            where:{id:workflowId},
+            include:{
+                nodes:true,
+                connection:true,
+            },
+        });
+
+        return topologicalSort(workflow.nodes,workflow.connection);
+       });
+
+       //Initialize context with any initail data from the trigger
+
+       let context=event.data.initialData || {};
+       for(const node of sortedNodes){
+        const executor=getExecutor(node.type as NodeType);
+        context=await executor({
+            data:node.data as Record<string,unknown>,
+            nodeId:node.id,
+            context,
+            step,
+        });
+       }
+       return {
+        workflowId,
+        result:context,
+       };
     },
 );
