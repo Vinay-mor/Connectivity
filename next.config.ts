2ac99d98b56369c9b1486a4a00aab3351be47945
Vinay-mor <vinaymor2005@gmail.com>
2026-07-21 19:20:46 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators:false,
+  outputFileTracingIncludes: {
+    "/*": ["src/generated/prisma/**/*"],
+  },
   async redirects(){
     return[
       {
